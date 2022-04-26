cebf4f27a0977e203d6480b782c2f1ebc513438f
Armando Rivera <rivera.armando997@gmail.com>
2022-04-26 16:13:50 -0500

    Show cards and superhero info

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,18 +1,14 @@
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <>
-      <h1>Hero List - {publisher}</h1>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>
-            {hero.superhero} - {hero.alter_ego}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="row row-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   );
 };
