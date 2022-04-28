d38c8cd0e8a54e06914253a4125b17b1d46af161
Armando Rivera <rivera.armando997@gmail.com>
2022-04-28 14:56:35 -0500

    Search component generated

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const Search = () => {
+  const [{ searchText }, handleInputChange, reset] = useForm({
+    searchText: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+    reset();
+  };
+
   return (
-    <div>
+    <>
       <h1>Search</h1>
-    </div>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Search</h4>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              onChange={handleInputChange}
+              value={searchText}
+            />
+
+            <button className="btn btn-outline-primary mt-3">Search</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
