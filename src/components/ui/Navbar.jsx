d065d71748789fb82f4bd6de0cb74ab7cd954bb2
Armando Rivera <rivera.armando997@gmail.com>
2022-04-29 20:04:47 -0500

    user login and set state and context provider

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const Navbar = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
     navigate("/login", { replace: true });
   };
+
+  const { user } = useContext(AuthContext);
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -44,7 +48,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Armando Rivera</span>
+          <span className="nav-item nav-link text-info">{user.name}</span>
           <button onClick={handleLogout} className="nav-item nav-link btn">
             Logout
           </button>
